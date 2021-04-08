057bdc28fed6f6cb567b6d8ee1550bfbf01168f1
SupremeCom <aleksandr_chichkanov@mail.ru>
2021-04-08 11:34:37 +0700

    Add files via upload

---
+++ zad2/zad2/Cycler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class Cycler
+    {
+
+        static int i = 0;             // глобальная переменная на весь класс, хранит нулевой индекс списка
+
+        public int method(ref int param)           // функция увеличивающая индекс списка на 1, вызывается при помощи ссылки
+        {
+            if (i <= 7)
+            {
+                i++;
+                return i;
+                
+            }
+            else
+            {
+                i = 0;
+                return i;
+            }
+            
+        }
+
+        public int method2(ref int param2)     // метод недающий счетчику быть больше размерности списка
+        {
+            if (i >= 8)
+                i = 0;
+            return i;
+        }
+
+
+
+       public int Output(List<int> numbers)        // сама функция для объекта класса выводящая 1 элемент списка
+        {
+         
+            
+                int result;
+            method2(ref i);
+            result = numbers[i];
+               method(ref i);
+                return result;
+            
+            
+        }
+    }
+}
---
+++ zad2/zad2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+
+
+   /* class Cycler // класс принимающий объекты
+    {
+        public int k;
+        List<int> numbers = new List<int>() { 1, 2, 3, 45, 7, 8, 23, 45 };
+
+        public void GetNext(int k)  // Метод который будет выводить элементы объекта в порядке их записи
+        {
+            var firstElement = numbers[0];
+            foreach (int i in numbers)           // цикл при помощи которого выводятся элементы объекта list "numbers"
+            {
+                Console.WriteLine(i + " - элементы объекта list");
+            }
+            for (int d = 0; d <= k; d++)
+            {
+                foreach (int i in numbers)
+                {
+                    Console.WriteLine(i);
+                    //Console.WriteLine(numbers[d]);
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+
+        int GetNext2(int number)
+        {
+            
+        }
+
+
+    }
+    */
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cycler data = new Cycler(); // инициализация объекта data класса Cycler
+            //Console.WriteLine("Введите сколько раз вывести элементы объекта list");
+            //data.k = Convert.ToInt32(Console.ReadLine()); // счетчик сколько элементов выводить
+            //data.GetNext(data.k);             // метод вывода информации
+
+            int result;
+            Cycler numb = new Cycler();
+            List<int> numbers1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+
+            Console.WriteLine("Введите кол-во вызовов функции");
+           int k;
+            k = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= k; i++)               // цикл вызова выводящей функции
+            {
+
+                result = numb.Output(numbers1);
+                Console.WriteLine(result);
+            }
+
+            
+            Console.ReadKey();
+
+        }
+    } 
+    
+}
